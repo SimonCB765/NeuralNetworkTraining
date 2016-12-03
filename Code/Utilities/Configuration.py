7767ba4e6ad22899f32acb758cc071fbb96e65cd
SimonCB765 <SimonCB765@gmail.com>
2016-12-03 11:23:44 +0000

    Added method to extract specific parameter

--- Code/Utilities/Configuration.py
+++ Code/Utilities/Configuration.py
@@ -24,6 +24,37 @@ class Configuration(object):
         self._configParams = {}
         self.set_from_dict(kwargs)
 
+    def get_param(self, path):
+        """Extract a configuration parameter from the dictionary of parameters.
+
+        :param path:    The path through the configuration parameter dictionary to use to extract the parameter.
+        :type path:     list[str]
+        :return:        The parameter's value or an indication that the parameter does not exist.
+        :rtype:         bool, list | int | str | float | dict | None
+                            1st element indicates whether the parameter was found.
+                            2nd element is the parameter's value if found and the name of the first missing dictionary
+                                key if not found (e.g. "A" if self._configParams["B"]["A"]["C"] fails because "A"
+                                is not a key in the self._configParams["B"] dictionary.
+
+        """
+
+        paramFound = False  # Whether the parameter was found.
+        paramValue = self._configParams  # The value to return.
+        for i in path:
+            print(paramValue.keys())
+            if i in paramValue:
+                # The next element in the path was found, so keep looking.
+                paramValue = paramValue[i]
+            else:
+                # The parameter was not found, so terminate the search.
+                paramValue = i
+                break
+        else:
+            # The parameter was found as all elements in the path were found.
+            paramFound = True
+
+        return paramFound, paramValue
+
     def set_from_dict(self, paramsToAdd):
         """Set configuration parameters from a dictionary of parameters.
 
