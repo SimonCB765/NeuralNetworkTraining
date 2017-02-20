b3c25511982121c62789e0f606c439fe62ddfbc3
sb0080 <s.c.bull@surrey.ac.uk>
2017-02-20 15:18:23 +0000

    Removed unneeded comma

--- Code/Network/InputPipeline/vector.py
+++ Code/Network/InputPipeline/vector.py
@@ -52,7 +52,7 @@ def main(dirShardedFiles, config):
     batcherCapacity = config.get_param(["TensorflowParams", "BatcherCapacity"])[1]
     batcherCapacity = max(batcherCapacity, batcherMinAfterDequeue + (numberThreads + 2) * batchSize)
     batchedSerialisedExamples = tf.train.shuffle_batch_join(
-        serialisedExamples, batchSize, capacity=batcherCapacity, min_after_dequeue=batcherMinAfterDequeue,
+        serialisedExamples, batchSize, capacity=batcherCapacity, min_after_dequeue=batcherMinAfterDequeue
     )
 
     # Define the feature mapping used for decoding examples. As we don't know the number of variables in advance, we
